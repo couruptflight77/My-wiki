960629a145d64113810df5e436c886b25b6fa75c
couruptflight77 <brockpoulson77@gmail.com>
2020-03-07 12:42:37 +1100

    legal3

--- wiki.js
+++ wiki.js
@@ -1,6 +1,7 @@
 var legal = document.querySelector("#LegaL_moves");
 localStorage.setItem("a3 a4 b3 b4 c3 c4 d3 d4 e3 e4 f3 f4 g3 g4 h3 h4 Nf3 Nh3 Na3 Nc3", "legal1")
 localStorage.setItem("a4 b3 b4 c3 c4 d3 d4 e3 e4 f3 f4 g3 g4 h3 h4 Nf3 Nh3 Nc3", "legal2")
+localStorage.setItem("a5 b3 b4 c3 c4 d3 d4 e3 e4 f3 f4 g3 g4 h3 h4 Nf3 Nc3 Na3 Nh3", "legal3")
 function CHessGame() {
     var Fm = prompt("Your move look bellow for legal moves")
     switch (Fm) {
